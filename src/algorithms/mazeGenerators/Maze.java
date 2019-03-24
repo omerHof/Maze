4c78cf49f440ec4a857c6197372668685d776110
roikatz16 <kroi@post.bgu.ac.il>
2019-03-24 17:57:49 +0200

    start

---
+++ src/algorithms/mazeGenerators/Maze.java
@@ -0,0 +1,21 @@
+package algorithms.mazeGenerators;
+
+import algorithms.Position;
+
+public class Maze {
+
+    public Position getStartPosition(){
+
+    }
+
+    public Position getGoalPosition(){
+
+    }
+
+    public void print (){
+
+    }
+    public void stam(){}
+
+
+}
